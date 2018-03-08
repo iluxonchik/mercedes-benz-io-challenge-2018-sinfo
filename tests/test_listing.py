be3f7d30476f131230b6a8ea612faa7e813b87e2
ILUXONCHIK <iluxon4ik@hotmail.com>
2018-03-08 00:42:51 +0000

    test: add tests to list vehicles by model

--- tests/test_listing.py
+++ tests/test_listing.py
@@ -1,5 +1,6 @@
 """Listing vehicles tests."""
 import unittest
+import json
 from mbio.testdrive import TestDrive
 
 
@@ -13,3 +14,13 @@ class TestListVehiles(unittest.TestCase):
         self.assertCountEqual(expected, obtained,
                               'Mismatch in expected (empty) and obtained list '
                               'of model Z vehicles query')
+
+    def test_list_vehicles_by_model(self):
+        EXPECTED_JSON_FILE_PATH = './tests/resources/expected_vehicles_model_e.json'
+        td = TestDrive(dataset='./tests/resources/dataset_full.json')
+        with open(EXPECTED_JSON_FILE_PATH, 'r') as f:
+            vehicle_list = json.load(f)
+        expected = vehicle_list['vehicles']
+        obtained = td.get_vehicles_by_model('E')
+        self.assertCountEqual(expected, obtained, 'Mismatch in obtained '
+                                                  'model E vehicle listing.')
