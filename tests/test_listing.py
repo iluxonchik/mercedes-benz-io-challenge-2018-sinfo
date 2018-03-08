739ff4589ae06b6667c537c9b6868a5d6c2b9336
ILUXONCHIK <iluxon4ik@hotmail.com>
2018-03-08 01:31:04 +0000

    test: add list vehicles by transmission tests

--- tests/test_listing.py
+++ tests/test_listing.py
@@ -43,7 +43,7 @@ class TestListVehiles(unittest.TestCase):
         obtained = td.get_vehicles_by_fuel_type('PENAUT BUTTER')
         self.assertCountEqual(expected, obtained,
                               'Mismatch in expected (empty) and obtained list '
-                              'of PENAUT BUTTER fuel type query')
+                              'of PENAUT BUTTER fuel type query.')
 
     def test_list_vehicles_by_fuel_type_electric(self):
         self.maxDiff = None
@@ -67,4 +67,37 @@ class TestListVehiles(unittest.TestCase):
         self.assertCountEqual(expected, obtained, 'Mismatch in obtained '
                                                   'ELECTRIC fuel type vehicle '
                                                   'listing when using mixed '
+                                                  'case.')
+
+    def test_list_vehicles_by_transmission_no_results(self):
+        td = TestDrive(dataset='./tests/resources/dataset_full.json')
+        expected = []
+        obtained = td.get_vehicles_by_transmission('THE DOCUMENTARY')
+        self.assertCountEqual(expected, obtained,
+                              'Mismatch in expected (empty) and obtained list '
+                              'of MANUAL transmission query.')
+
+    def test_list_vehicles_by_transmission_manual(self):
+        self.maxDiff = None
+        EXPECTED_JSON_FILE_PATH = './tests/resources/expected_vehicles_transmission_manual.json'
+        td = TestDrive(dataset='./tests/resources/dataset_full.json')
+        with open(EXPECTED_JSON_FILE_PATH, 'r') as f:
+            vehicle_list = json.load(f)
+        expected = vehicle_list['vehicles']
+        obtained = td.get_vehicles_by_transmission('MANUAL')
+        self.assertCountEqual(expected, obtained, 'Mismatch in obtained '
+                                                  'MANUAL transmission vehicle '
+                                                  'listing.')
+
+    def test_list_vehicles_by_transmission_manual_mixed_case(self):
+        self.maxDiff = None
+        EXPECTED_JSON_FILE_PATH = './tests/resources/expected_vehicles_transmission_manual.json'
+        td = TestDrive(dataset='./tests/resources/dataset_full.json')
+        with open(EXPECTED_JSON_FILE_PATH, 'r') as f:
+            vehicle_list = json.load(f)
+        expected = vehicle_list['vehicles']
+        obtained = td.get_vehicles_by_transmission('mANUAl')
+        self.assertCountEqual(expected, obtained, 'Mismatch in obtained '
+                                                  'MANUAL transmission vehicle'
+                                                  ' listing when using mixed '
                                                   'case.')
