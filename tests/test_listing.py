a9233a30030ccdf7fd121f441c33d36451999747
ILUXONCHIK <iluxon4ik@hotmail.com>
2018-03-08 00:58:22 +0000

    test: add test for getting vehicles by non-exisitng fuel type

--- tests/test_listing.py
+++ tests/test_listing.py
@@ -24,3 +24,23 @@ class TestListVehiles(unittest.TestCase):
         obtained = td.get_vehicles_by_model('E')
         self.assertCountEqual(expected, obtained, 'Mismatch in obtained '
                                                   'model E vehicle listing.')
+
+    def test_list_vehicles_by_model_case_mixture(self):
+        EXPECTED_JSON_FILE_PATH = './tests/resources/expected_vehicles_model_e.json'
+        td = TestDrive(dataset='./tests/resources/dataset_full.json')
+        with open(EXPECTED_JSON_FILE_PATH, 'r') as f:
+            vehicle_list = json.load(f)
+        expected = vehicle_list['vehicles']
+        obtained = td.get_vehicles_by_model('e')
+        self.assertCountEqual(expected, obtained, 'Mismatch in obtained '
+                                                  'model E vehicle listing '
+                                                  'when using mixed letter '
+                                                  'case.')
+
+    def test_list_vehicles_by_fuel_type_no_results(self):
+        td = TestDrive(dataset='./tests/resources/dataset_full.json')
+        expected = []
+        obtained = td.get_vehicles_by_fuel_type('PENAUT BUTTER')
+        self.assertCountEqual(expected, obtained,
+                              'Mismatch in expected (empty) and obtained list '
+                              'of PENAUT BUTTER fuel type query')
