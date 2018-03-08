d58e347e0afe4280119683d9568efb7199f5082c
ILUXONCHIK <iluxon4ik@hotmail.com>
2018-03-08 01:58:21 +0000

    refactor: simplify exception classes

--- mbio/exceptions.py
+++ mbio/exceptions.py
@@ -1,15 +1,7 @@
 class TestDriveError(Exception):
     """Basic exception for all errors raised by the TestDrive app."""
-    
-    def __init__(self, msg=None):
-        if msg is None:
-            msg = 'An error in the TestDrive app occured.'
-        super(TestDriveError, self).__init__(msg)
+    pass
 
 class InvalidDataSetError(TestDriveError):
     """The provided JSON data set is invalid."""
-
-    def __init__(self, msg=None):
-        if msg is None:
-            msg = 'Error loading the dataset.'
-        super(InvalidDataSetError, self).__init__(msg)
+    pass
