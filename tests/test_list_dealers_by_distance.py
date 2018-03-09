5ff36b033d7804277de005320577756e17c2c997
ILUXONCHIK <iluxon4ik@hotmail.com>
2018-03-09 21:45:46 +0000

    test: add tests for closest dealer

---
+++ tests/test_list_dealers_by_distance.py
@@ -0,0 +1,42 @@
+"""Listing dealers by distance tests."""
+
+import unittest
+import json
+from mbio.testdrive import TestDrive
+
+
+class TestListDealersByDistance(unittest.TestCase):
+    """List vehicle by features tests."""
+
+    LOC_1 = (38.187787, -8.104157)
+    LOC_2 = (50.26487, 28.67669)
+
+    def test_get_closest_dealer_no_results(self):
+        td = TestDrive(dataset='./tests/resources/dataset_full.json')
+        expected = None
+        obtained = td.get_closest_dealer_with_vehicle(latitude=self.LOC_1[0],
+            self.LOC_1[1], model='E', fuel='air', transmission='manual')
+        self.assertIsNone(obtained, 'Dealer has been returned, when none '
+                                    'should have.')
+
+    def test_get_closest_dealer_two_matches(self):
+        # REGEX: "model": "E",\n\t+"fuel": "ELECTRIC",\n\t+"transmission": "AUTO"
+        td = TestDrive(dataset='./tests/resources/dataset_full.json')
+        with open(EXPECTED_JSON_FILE_PATH, 'r') as f:
+            dealers_list = json.load(f)
+        expected = dealers_list['dealers'][0]
+        obtained = td.get_closest_dealer_with_vehicle(latitude=self.LOC_1[0],
+            self.LOC_1[1], model='E', fuel='electric', transmission='auto')
+        self.assertCountEqual(expected, obtained, 'Wrong closest dealer '
+                                                  'returned.')
+
+
+    def test_get_closest_dealer_one_match(self):
+        td = TestDrive(dataset='./tests/resources/dataset_full.json')
+        with open(EXPECTED_JSON_FILE_PATH, 'r') as f:
+            dealers_list = json.load(f)
+        expected = dealers_list['dealers'][0]
+        obtained = td.get_closest_dealer_with_vehicle(latitude=self.LOC_2[0],
+            self.LOC_2[1], model='AMG', fuel='gasoline', transmission='manual')
+        self.assertCountEqual(expected, obtained, 'Wrong closest dealer '
+                                                  'returned.')
