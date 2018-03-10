aed83d6c7842d81634fc144c81d8e2d94c485e28
ILUXONCHIK <iluxon4ik@hotmail.com>
2018-03-10 02:13:46 +0000

    test: add tests for getting list of dealers that have a vehicle sorted by distance

--- tests/test_list_dealers_by_distance.py
+++ tests/test_list_dealers_by_distance.py
@@ -146,7 +146,6 @@ class TestListDealersByDistance(unittest.TestCase):
                                                   'returned.')
 
     def test_get_closest_dealer_fuel_and_transmission_only(self):
-        self.maxDiff = None
         EXPECTED_JSON_FILE_PATH  = './tests/resources/expected_closest_dealer_porto.json'
         td = TestDrive(dataset='./tests/resources/dataset_full.json')
         with open(EXPECTED_JSON_FILE_PATH, 'r') as f:
@@ -162,3 +161,46 @@ class TestListDealersByDistance(unittest.TestCase):
             self.LOC_2[1], model=None, fuel=None, transmission='auto')
         self.assertEqual(expected, obtained, 'Wrong closest dealer '
                                                   'returned.')
+
+    def test_get_closest_dealers_no_results(self):
+        td = TestDrive(dataset='./tests/resources/dataset_full.json')
+        expected = []
+        obtained = td.get_closest_dealers_with_vehicle(latitude=self.LOC_1[0],
+            longitude=self.LOC_1[1], model='E', fuel='air', transmission='manual')
+        self.assertCountEqual(expected, obtained, 'Non-emtpy dealer list '
+                                                  'returned.')
+
+    def test_get_closest_dealers_one_match(self):
+        EXPECTED_JSON_FILE_PATH  = './tests/resources/dataset_full_modified_albufeira.json'
+        td = TestDrive(dataset='./tests/resources/dataset_full_modified_albufeira.json')
+        with open(EXPECTED_JSON_FILE_PATH, 'r') as f:
+            dealers_list = json.load(f)
+        expected = [dealers_list['dealers'][0]]
+        obtained = td.get_closest_dealers_with_vehicle(latitude=self.LOC_2[0],
+            longitude=self.LOC_2[1], model='A', fuel='B', transmission='C')
+        self.assertEqual(expected, obtained, 'Wrong closest dealer '
+                                                  'returned.')
+
+    def test_get_closest_dealers_two_matches_close(self):
+        # REGEX: "model": "E",\n\t+"fuel": "ELECTRIC",\n\t+"transmission": "AUTO"
+        EXPECTED_JSON_FILE_PATH  = './tests/resources/dataset_full_modified_porto_albufeira.json'
+        td = TestDrive(dataset='./tests/resources/dataset_full_modified_porto_albufeira.json')
+        with open(EXPECTED_JSON_FILE_PATH, 'r') as f:
+            dealers_list = json.load(f)
+        expected = [dealers_list['dealers'][0], dealers_list['dealers'][2]]
+        obtained = td.get_closest_dealers_with_vehicle(self.LOC_1[0],
+            self.LOC_1[1], 'A', 'B', 'C')
+        self.assertEqual(expected, obtained, 'Wrong closest dealer '
+                                             'returned.')
+
+    def test_get_closest_dealers_two_matches_far_away(self):
+        # REGEX: "model": "E",\n\t+"fuel": "ELECTRIC",\n\t+"transmission": "AUTO"
+        EXPECTED_JSON_FILE_PATH  = './tests/resources/dataset_full_modified_porto_albufeira.json'
+        td = TestDrive(dataset='./tests/resources/dataset_full_modified_porto_albufeira.json')
+        with open(EXPECTED_JSON_FILE_PATH, 'r') as f:
+            dealers_list = json.load(f)
+        expected = [dealers_list['dealers'][2], dealers_list['dealers'][0]]
+        obtained = td.get_closest_dealers_with_vehicle(self.LOC_2[0],
+            self.LOC_2[1], 'A', 'B', 'C')
+        self.assertEqual(expected, obtained, 'Wrong closest dealer '
+                                             'returned.')
