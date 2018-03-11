79adb799d98eb91a948085124a5c67615399e001
ILUXONCHIK <iluxon4ik@hotmail.com>
2018-03-11 20:55:35 +0000

    test: add test for adding a booking

--- tests/test_bookings.py
+++ tests/test_bookings.py
@@ -1,26 +1,62 @@
 """Booking creation and cancelling tests."""
 import unittest
+from unittest.mock import patch
 from mbio.testdrive import TestDrive
 import datetime
+import uuid
 
-class BookingsTestCase(object):
+MOCKED_UUIDS = ['136fbb51-8a06-42fd-b839-d01ab87e2c6c', '136fbb51-8a06-42fd-b839-c01ab87e2c6b',
+'132fbb51-8a06-42fd-b839-c01ab87e2c6c']
+
+class MockedDateTime(datetime.datetime):
+    MOCKED_DATE_VALUE = datetime.datetime(2018, 10, 3, 19, 22, 19, 92)
+    @classmethod
+    def today(cls):
+        return cls.MOCKED_DATE_VALUE
+
+class BookingsTestCase(unittest.TestCase):
     """Tests related to making and cancelling bookings."""
     #
     # When doing a booking:
     #     1. make sure vehicle_id exists (else: Exception)
-    #     2. make sure delaer that has the vehicle_id (else: Exception)
-    #     3. make sure vehicle is available on day/time (else: Exception)
+    #     ------- Below handeled by BookingDate Class ----
+    #     2. make sure vehicle is available on day/time (else: Exception)
     #             (else: Exception)
-    #     4. make sure that that vehicle_id is not registered for that date already,
+    #     3. make sure that that vehicle_id is not registered for that date already,
     #             UNLESS booking is cancelled(else: Exception)
-    #     5. OK: create booking
+    #     4. OK: create booking
     #
 
-    def test_booking_success(self):
-        return True
+    @classmethod
+    def setUpClass(cls):
+        datetime.datetime = MockedDateTime
+
+    @patch.object(uuid, 'uuid4', side_effect=MOCKED_UUIDS)
+    def test_booking_success(self, uuid):
         td = TestDrive(dataset='./tests/resources/dataset_full.json')
+        vehicle_id = '136fbb51-8a06-42fd-b839-c01ab87e2c6c'
         # reservation for April 9th at 10:00 (Tuesday)
-        pikup_date = datetime.datetime(2019, 4, )
-        td.create_booking(firstName='Jayceon', lastName='Taylor',
-                          vehicleId='136fbb51-8a06-42fd-b839-c01ab87e2c6c',
-                          pickupDate=sad)
+        pickup_date = datetime.datetime(2019, 4, 9, 10, 00)
+        result = td.create_booking(first_name='Jayceon', last_name='Taylor',
+                          vehicle_id=vehicle_id,
+                          pickup_date=pickup_date)
+
+        # now, make sure that a booking object was created and added
+        UUID_stub = MOCKED_UUIDS[0]
+
+        expected_booking = {
+                    		"id": UUID_stub,
+                    		"firstName": "Jayceon",
+                    		"lastName": "Taylor",
+                    		"vehicleId": vehicle_id,
+                    		"pickupDate": pickup_date.isoformat(),
+                    		"createdAt": MockedDateTime.MOCKED_DATE_VALUE
+                    }
+        obtained_booking = result
+        self.assertIsNotNone(obtained_booking, 'No booking returned')
+        self.assertEqual(expected_booking, obtained_booking,
+                         'Erroneous booking obtained.')
+        dataset = td._dataset
+        bookings = dataset['bookings']
+        self.assertTrue(obtained_booking in bookings, 'New booking was not '
+                        'added to the dataset.')
