019405b3da314cbe1e51e7a8f78dcc6a2614fc32
ILUXONCHIK <iluxon4ik@hotmail.com>
2018-03-13 18:46:15 +0000

    test: add server tests

--- tests/test_server.py
+++ tests/test_server.py
@@ -5,7 +5,6 @@ import threading
 import json
 import urllib.request
 import urllib.parse
-from urllib.parse import urlencode
 from urllib.request import Request, urlopen
 from urllib.error import HTTPError
 import datetime
@@ -15,8 +14,9 @@ from unittest.mock import patch
 
 from mbio.server.endpoint import Endpoint
 from mbio.server.server import Server
-from http.server import HTTPServer
 
+MOCKED_UUIDS = ['136fbb51-8a06-42fd-b839-d01ab87e2c6c', '136fbb51-8a06-42fd-b839-c01ab87e2c6b',
+'132fbb51-8a06-42fd-b839-c01ab87e2c6c']
 
 class TestHTTPServer(HTTPServer):
 
@@ -29,9 +29,6 @@ class MockedDateTime(datetime.datetime):
     @classmethod
     def today(cls):
         return cls.MOCKED_DATE_VALUE
-
-MOCKED_UUIDS = ['136fbb51-8a06-42fd-b839-d01ab87e2c6c', '136fbb51-8a06-42fd-b839-c01ab87e2c6b',
-'132fbb51-8a06-42fd-b839-c01ab87e2c6c']
 
 class RESTServerTestCase(unittest.TestCase):
     SERVER_PORT = 1234
@@ -51,6 +48,60 @@ class RESTServerTestCase(unittest.TestCase):
     def tearDown(self):
         self.httpd.shutdown()
         self.thr.join()
+
+    def test_server_crashes_on_invalid_dataset(self):
+        with self.assertRaises(Exception):
+            Server.td = None
+            Server.DATASET_PATH = 'Still Dre Day'
+            Server()
+
+    def test_server_error_get_on_post(self):
+        expected = {
+                    "error": "This endpoint only supports the POST method."
+                   }
+        res = self._get_request('{}'.format(Endpoint.BOOKINGS_CREATE))
+        obtained = json.loads(res)
+
+        self.assertEqual(expected, obtained)
+
+    def test_server_error_post_on_get(self):
+        expected = {
+                    "error": "This endpoint only supports the GET method."
+                   }
+        res = self._post_request('{}'.format(Endpoint.VEHICLES), {'a':'b'})
+        obtained = json.loads(res)
+
+        self.assertEqual(expected, obtained)
+
+    def test_unknown_endpoint_get_error(self):
+        expected =  {
+                    "error": "Unknown endpoint."
+                    }
+
+        res = self._get_request('{}'.format('/the-documentary/album/'))
+        obtained = json.loads(res)
+
+        self.assertEqual(expected, obtained)
+
+    def test_unknown_endpoint_post_error(self):
+        expected =  {
+                    "error": "Unknown endpoint."
+                    }
+
+        res = self._post_request('{}'.format('/the-the-chronic/album/'), {})
+        obtained = json.loads(res)
+
+        self.assertEqual(expected, obtained)
+
+    def test_unknown_endpoint_put_error(self):
+        expected =  {
+                    "error": "Unknown endpoint."
+                    }
+
+        res = self._put_request('{}'.format('/the-the-chronic/album/'), {})
+        obtained = json.loads(res)
+
+        self.assertEqual(expected, obtained)
 
     def test_get_vehicles(self):
         EXPECTED_JSON_FILE_PATH = './tests/resources/expected_all_vehicles.json'
@@ -96,15 +147,55 @@ class RESTServerTestCase(unittest.TestCase):
 
         self.assertEqual(expected, obtained)
 
+    def test_create_booking_invalid_date(self):
+        expected = {
+                "error": "2019-04-08a10:30:00 is not a valid ISO date format"
+        }
+
+        data = {
+                "first_name": "Jayceon",
+                "last_name": "Taylor",
+                "vehicle_id": "136fbb51-8a06-42fd-b839-c01ab87e2c6c",
+                "pickup_date": "2019-04-08a10:30:00"
+               }
+
+        res = self._post_request('{}'.format(Endpoint.BOOKINGS_CREATE), data)
+        obtained = json.loads(res)
+        self.assertEqual(expected, obtained)
+
+    def test_cancel_booking(self):
+        data = {
+                  "booking_id": "b00d3e76-9605-49c7-910b-03b51679f6d6",
+                  "reason": "On a scale from one to ten I'm a nine, with two M's"
+               }
+
+        res = self._put_request('{}'.format(Endpoint.BOOKINGS_CANCEL), data)
+        obtained = json.loads(res)
+
+        expected = {
+                    "id": "b00d3e76-9605-49c7-910b-03b51679f6d6",
+                    "firstName": "Joanna",
+                    "lastName": "Walker",
+                    "vehicleId": "875f00fa-9f67-44ea-bb26-75ff375fdd3f",
+                    "pickupDate": "2018-03-03T10:30:00",
+                    "createdAt": "2018-02-26T08:42:46.298",
+                    "cancelledAt": MockedDateTime.MOCKED_DATE_VALUE.isoformat(),
+                    "cancelledReason": "On a scale from one to ten I'm a nine, with two M's"
+                }
+
+        self.assertEqual(expected, obtained)        
+
     def _get_request(self, endpoint):
         url = 'http://localhost:{}{}'.format(RESTServerTestCase.SERVER_PORT, endpoint)
         req = urllib.request.Request(url)
 
-        with urllib.request.urlopen(req) as response:
-            res = response.read()
+        try:
+            with urllib.request.urlopen(req) as response:
+                json_res = response.read().decode()
+        except HTTPError as e:
+            json_res = e.read().decode()
 
-        res = res.decode('utf-8')
-        return res
+        return json_res
 
     def _post_request(self, endpoint, data):
 
@@ -114,8 +205,18 @@ class RESTServerTestCase(unittest.TestCase):
             request = Request(url, json.dumps(data).encode())
             json_res = urlopen(request).read().decode()
         except HTTPError as e:
-            print('ERROR:\n\n\n')
-            content = e.read()
-            print(content)
+            json_res = e.read().decode()
+
+        return json_res
+
+    def _put_request(self, endpoint, data):
+
+        url = 'http://localhost:{}{}'.format(RESTServerTestCase.SERVER_PORT, endpoint)
+
+        try:
+            request = Request(url, json.dumps(data).encode(), method='PUT')
+            json_res = urlopen(request).read().decode()
+        except HTTPError as e:
+            json_res = e.read().decode()
 
         return json_res
