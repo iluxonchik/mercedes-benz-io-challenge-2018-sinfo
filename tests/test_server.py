737435b9f8d78f61aab8a11ca7817278c6469265
ILUXONCHIK <iluxon4ik@hotmail.com>
2018-03-13 15:15:01 +0000

    test: add POST requests to tests; add create booking test

--- tests/test_server.py
+++ tests/test_server.py
@@ -4,7 +4,14 @@ import unittest
 import threading
 import json
 import urllib.request
+import urllib.parse
+from urllib.parse import urlencode
+from urllib.request import Request, urlopen
+from urllib.error import HTTPError
+import datetime
+import uuid
 from http.server import HTTPServer
+from unittest.mock import patch
 
 from mbio.server.endpoint import Endpoint
 from mbio.server.server import Server
@@ -17,9 +24,21 @@ class TestHTTPServer(HTTPServer):
         super(TestHTTPServer, self).shutdown()
         self.server_close()
 
+class MockedDateTime(datetime.datetime):
+    MOCKED_DATE_VALUE = datetime.datetime(2018, 10, 3, 19, 22, 19, 92)
+    @classmethod
+    def today(cls):
+        return cls.MOCKED_DATE_VALUE
+
+MOCKED_UUIDS = ['136fbb51-8a06-42fd-b839-d01ab87e2c6c', '136fbb51-8a06-42fd-b839-c01ab87e2c6b',
+'132fbb51-8a06-42fd-b839-c01ab87e2c6c']
 
 class RESTServerTestCase(unittest.TestCase):
     SERVER_PORT = 1234
+
+    @classmethod
+    def setUpClass(cls):
+        datetime.datetime = MockedDateTime
 
     def setUp(self):
         Server.DATASET_PATH = './tests/resources/dataset_full.json'
@@ -53,6 +72,30 @@ class RESTServerTestCase(unittest.TestCase):
 
         self.assertEqual(expected, obtained)
 
+    @patch.object(uuid, 'uuid4', side_effect=MOCKED_UUIDS)
+    def test_create_booking(self, uuid):
+
+        data = {
+                "first_name": "Jayceon",
+                "last_name": "Taylor",
+                "vehicle_id": "136fbb51-8a06-42fd-b839-c01ab87e2c6c",
+                "pickup_date": "2019-04-08T10:30:00"
+               }
+
+        res = self._post_request('{}'.format(Endpoint.BOOKINGS_CREATE), data)
+        obtained = json.loads(res)
+
+        expected = {
+            "id": MOCKED_UUIDS[0],
+            "firstName": "Jayceon",
+            "lastName": "Taylor",
+            "vehicleId": "136fbb51-8a06-42fd-b839-c01ab87e2c6c",
+            "pickupDate": "2019-04-08T10:30:00",
+            "createdAt":  MockedDateTime.MOCKED_DATE_VALUE.isoformat(),
+        }
+
+        self.assertEqual(expected, obtained)
+
     def _get_request(self, endpoint):
         url = 'http://localhost:{}{}'.format(RESTServerTestCase.SERVER_PORT, endpoint)
         req = urllib.request.Request(url)
@@ -62,3 +105,17 @@ class RESTServerTestCase(unittest.TestCase):
 
         res = res.decode('utf-8')
         return res
+
+    def _post_request(self, endpoint, data):
+
+        url = 'http://localhost:{}{}'.format(RESTServerTestCase.SERVER_PORT, endpoint)
+
+        try:
+            request = Request(url, json.dumps(data).encode())
+            json_res = urlopen(request).read().decode()
+        except HTTPError as e:
+            print('ERROR:\n\n\n')
+            content = e.read()
+            print(content)
+
+        return json_res
